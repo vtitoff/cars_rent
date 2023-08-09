1c719c2b8028bf78679458f4eb35e3f8e5e6f3e4
vtitoff <v.titoff2010@gmail.com>
2023-08-09 16:44:10 +0300

    add deal model and car models

--- cars_app/admin.py
+++ cars_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Car, User, Deal, Manufacturer, CarModel
 
 # Register your models here.
+admin.site.register(Car)
+admin.site.register(User)
+admin.site.register(Deal)
+admin.site.register(Manufacturer)
+admin.site.register(CarModel)
--- cars_app/models.py
+++ cars_app/models.py
@@ -1,12 +1,28 @@
 from django.db import models
+import uuid
+from enum import Enum
 
 
-# Create your models here.
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=100, primary_key=True)
+
+    def __str__(self):
+        return self.name
+
+
+class CarModel(models.Model):
+    name = models.CharField(max_length=100, primary_key=True)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.manufacturer} {self.name}"
+
+
 class Car(models.Model):
-    car_id = models.CharField(max_length=200, primary_key=True)
-    license_number = models.CharField(max_length=200)
-    manufacturer = models.CharField(max_length=200)
-    car_model = models.CharField(max_length=200)
+    car_id = models.CharField(max_length=200, primary_key=True, default=uuid.uuid4())
+    license_number = models.CharField(max_length=50)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    car_model = models.ForeignKey(CarModel, on_delete=models.CASCADE)
     year_of_production = models.IntegerField()
     odometer = models.FloatField()
     in_rent = models.BooleanField()
@@ -15,31 +31,38 @@ class Car(models.Model):
     updated_time = models.DateTimeField()
 
     def __str__(self):
-        return f'car_id: {self.car_id}, car_model: {self.manufacturer} {self.car_model}, in_rent: {self.in_rent}'
+        return f"{self.car_model} {self.license_number}, in_rent: {self.in_rent}, car_id: {self.car_id}"
 
 
 class User(models.Model):
-    user_id = models.CharField(max_length=200, primary_key=True)
+    user_id = models.CharField(max_length=200, primary_key=True, default=uuid.uuid4())
     user_phone = models.CharField(max_length=50)
-    first_name = models.CharField(max_length=200)
-    middle_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
-    driver_license = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    driver_license = models.CharField(max_length=50)
     debt = models.FloatField()
 
     def __str__(self):
-        return f'user_id:{self.user_id}, name: {self.first_name} {self.last_name}'
+        return f"user_id:{self.user_id}, name: {self.first_name} {self.last_name}"
 
 
 class Deal(models.Model):
-    deal_id = models.CharField(max_length=200, primary_key=True)
+    STATUSES = (
+        ("NEW", "new"),
+        ("IN_PROGRESS", "in_progress"),
+        ("CANCELLED", "cancelled"),
+        ("FINISHED", "finished"),
+    )
+
+    deal_id = models.CharField(max_length=200, primary_key=True, default=uuid.uuid4())
     duration_hours = models.IntegerField()
     cost = models.FloatField()
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     car_id = models.ForeignKey(Car, on_delete=models.CASCADE)
+    status = models.CharField(max_length=100, choices=STATUSES, default="NEW")
 
     def __str__(self):
-        return f'deal_id: {self.deal_id}, user_id: {self.user_id}, cost: {self.cost}'
-
+        return f"deal_id: {self.deal_id}, user_id: {self.user_id}, cost: {self.cost}"
